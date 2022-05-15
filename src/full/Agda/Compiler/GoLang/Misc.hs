54ca82e3f34487dade10fe2ea4fbd2911c0f1184
Gintautas <gintautas@igem.org>
2022-05-16 01:11:34 +0200

    Move misc encoding functions from compiler to Misc

--- src/full/Agda/Compiler/GoLang/Misc.hs
+++ src/full/Agda/Compiler/GoLang/Misc.hs
@@ -5,6 +5,11 @@ import qualified Agda.Syntax.Treeless as T
 import Agda.Compiler.Backend
 import Agda.TypeChecking.Monad.Base
 import Agda.Utils.Pretty
+
+import Data.Char (isAsciiUpper, isLetter, isDigit, ord)
+
+-- import qualified Agda.Compiler.GoLang.Syntax as GS
+-- import Agda.Syntax.Abstract.Name ( QName )
  
 divider = "\n================================================================================\n"
 
@@ -22,3 +27,36 @@ d <+%!> a = returnTCMT $ d <+> (text . show) a
 
 mapBoolToArgUsage :: Bool -> T.ArgUsage
 mapBoolToArgUsage b = if b then T.ArgUnused else T.ArgUsed
+
+encode :: [Char] -> String
+encode []              = []
+encode name@(c : tail) = do
+  case isAsciiUpper c of
+    True  -> encodeChars (name)
+    False -> encodeChars $ 'F' : name
+
+encodeChars :: [Char] -> String
+encodeChars (c : tail) = (encodeChar c) ++ (encodeChars tail)
+encodeChars []         = []
+
+encodeChar :: Char -> String
+encodeChar c = do
+  case isValidChar c of
+    True  -> [c]
+    False -> "u" ++ (show (ord c))
+
+isValidChar :: Char -> Bool
+isValidChar c = (isLetter c) || ('_' == c) || (isDigit c)
+
+-- unqgname :: NameKind -> QName -> GS.Name
+-- unqhname k q =
+--   GS.Ident $ encodeString k $
+--     "_" ++ prettyShow (nameCanonical n) ++ "_" ++ idnum (nameId n)
+--   where
+--   n = qnameName q
+
+--   idnum (NameId x _) = show (fromIntegral x)
+
+
+-- dname :: QName -> GS.Name
+-- dname q = unqgname (FunK PossiblyUnused) q

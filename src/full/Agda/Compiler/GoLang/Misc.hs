d0d001ef763bfd11c2e376ae80982b699bd7431b
Gintautas <gintautas@igem.org>
2022-05-19 18:45:15 +0200

    Rewrite reportSDocDivided function to work with lambda tree printing. Add underscore after compiled function name.

--- src/full/Agda/Compiler/GoLang/Misc.hs
+++ src/full/Agda/Compiler/GoLang/Misc.hs
@@ -2,7 +2,7 @@ module Agda.Compiler.GoLang.Misc where
 
 import qualified Agda.Syntax.Treeless as T
 
-import Agda.Compiler.Backend
+import qualified Agda.Compiler.Backend as CB
 import Agda.TypeChecking.Monad.Base
 import Agda.Utils.Pretty
 
@@ -15,8 +15,15 @@ divider = "\n===================================================================
 
 -- prints debug message to console beautifully, specifying it's key and level, separating with dividers
 -- EXAMPLE USAGE in CLI: `agda-2.6.2.2-golang --go -v function.go:20 compilationTests.agda`
-reportSDocDivided :: MonadDebug m => VerboseKey -> VerboseLevel -> TCM Doc -> m ()
-reportSDocDivided vk vl tdoc = reportSDoc vk vl $ (\x -> ("\nGO_COMPILER_DEBUG_LOG\nV_KEY:" <+> (text.show) vk <+> "\nV_LEVEL:" <+> (text.show) vl <+> divider <+> x <+> divider)) <$> tdoc
+-- reportSDocDivided :: MonadDebug m => VerboseKey -> VerboseLevel -> TCM Doc -> m ()
+-- reportSDocDivided vk vl tdoc = reportSDoc vk vl $ (\x -> ("\nGO_COMPILER_DEBUG_LOG\nV_KEY:" <+> (text.show) vk <+> "\nV_LEVEL:" <+> (text.show) vl <+> divider <+> x <+> divider)) <$> tdoc
+-- reportSDocDivided vk vl tdoc = reportSDoc vk vl $ (\x -> x ) <$> tdoc
+
+reportS :: CB.MonadDebug m => CB.VerboseKey -> CB.VerboseLevel -> TCM Doc -> m ()
+reportS vk vl tdoc = do
+  CB.reportS vk vl $ "\nGO_COMPILER_DEBUG_LOG\nV_KEY:" <+> (text.show) vk <+> "\nV_LEVEL:" <+> (text.show) vl <+> divider
+  CB.reportS vk vl tdoc
+  CB.reportS vk vl $ divider
 
 -- aliases of functions for showing intermediate compilation values
 (<+%>) :: (Pretty a) => Doc -> a -> TCM Doc
@@ -31,7 +38,7 @@ mapBoolToArgUsage b = if b then T.ArgUnused else T.ArgUsed
 encode :: [Char] -> String
 encode []              = []
 encode name@(c : tail) = do
-  if isAsciiUpper c then encodeChars (name) else encodeChars $ 'F' : name
+  if isAsciiUpper c then encodeChars (name) else encodeChars $ "F_" <> name
 
 encodeChars :: [Char] -> String
 encodeChars tail = concatMap encodeChar tail
